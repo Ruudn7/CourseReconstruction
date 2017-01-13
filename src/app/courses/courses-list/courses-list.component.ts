464d51b7ce4638c5d2b9d0b54e3c778086aa245f
Unknown <ruudn7@wp.pl>
2017-01-13 19:47:26 +0100

    course-listFinish

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Courses} from '../courses';
+import {User} from '../../users/user';
+
+import { CoursesService } from '../courses.service';
+import { UsersService } from '../../users/users.service';
+
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesListComponent implements OnInit {
 
-  constructor() { }
+  private courses: Courses[];
+  private users: User[];
+  private errorMessage: string;
+  constructor(
+    private coursesService: CoursesService,
+    private usersService: UsersService,
+  ){}
 
-  ngOnInit() {
+  ngOnInit() {                                     //funkcja która odpala się jako pierwsza
+    this.usersService.getUsers()                    //wywołanie z backendu UsersService i funkcji getUser która zwraca adres konta jakiegoś użytkownika
+    .subscribe(
+      users => this.users = users,
+      error => this.errorMessage = <any>error
+    )
+
+    this.coursesService.getCourses()            //wywołanie z backendu UsersService i funkcji getCourses która zwraca dane konkretnego kursu
+      .subscribe(
+        courses => this.courses = courses,
+        error => this.errorMessage = <any>error
+      )
   }
 
+
+
+  checkSomethink(id) {                                                              //pobiera id użytkownika chef
+
+    for(let i=0; i < this.users.length; i++) {                                      // iteruje po liście prowadzących kurs
+      if(this.users[i]._id === id) {                                                //jeżeli id danego użytkownika się zgadza wyświetla jego imię i nazwisko
+        return this.users[i].name + ' ' + this.users[i].surname;
+      }
+    }
+    return 'Nie znaleziono użytkownika';
+  }
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -1,3 +1,6 @@
 export interface User{
     _id?: string;
+    user: string;
+    name?: string;
+    surname?: string;
 }
