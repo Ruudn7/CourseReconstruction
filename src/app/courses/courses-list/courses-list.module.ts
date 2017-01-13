1bbbb70479b40fcff430cba42fd32e9f7d8246f6
Unknown <ruudn7@wp.pl>
2017-01-13 19:54:47 +0100

    course-listFinish2

--- src/app/courses/courses-list/courses-list.module.ts
+++ src/app/courses/courses-list/courses-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoursesListComponent } from './courses-list.component';
 
 @NgModule({
   imports: [
