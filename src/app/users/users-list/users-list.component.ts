6a68c288a5ce1790f7941a11763753f0a60ac455
Unknown <ruudn7@wp.pl>
2017-01-16 14:39:28 +0100

    userListComponent

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+
+import { User } from '../user';
+import { Inspektorat } from '../inspektorat/inspektorat';
+
 
 @Component({
   selector: 'app-users-list',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
+  errorMessage: string;
+  users: User[];
+  private inspektoraty: Inspektorat[];
 
-  constructor() { }
+  constructor (private usersService) { }
 
-  ngOnInit() {
+
+
+  getUsers() {
+    this.usersService.getUsers()
+    .subscribe(
+      users => this.users = users,
+      error => this.errorMessage = <any>error
+    );
   }
 
+  getInspektoraty() {
+    this.usersService.getInspektorat()
+      .subscribe(
+        inspektoraty => this.inspektoraty = inspektoraty
+      )
+  }
+
+  ngOnInit() {
+    this.getUsers()
+    this.getInspektoraty();
+  }
+
+giveNameInspektorat(id) {
+
+  for(let i = 0; i< this.inspektoraty.length; i++){
+    if(id === this.inspektoraty[i]._id)
+      return this.inspektoraty[i].name;
+  }
+
+    return 'Brak';
+  }
 }
