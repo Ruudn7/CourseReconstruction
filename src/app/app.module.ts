edc1c3172136af883f49c28c6f286bb83ce56932
Unknown <ruudn7@wp.pl>
2017-01-17 00:13:18 +0100

    DisableChefPlusAppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,57 @@
 
-import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap'
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
 
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+import { RoutingModule, routableComponents } from './routing/routing.module';
+
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
 import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
-import { LoginComponent } from './login/login.component';
-import { RoutingComponent } from './routing/routing.component';
-import { UsersComponent } from './users/users.component';
+
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module'
+
+import { UsersService } from './users/users.service';
+import { LoginService } from './login/login.service';
+import { CoursesService } from './courses/courses.service';
+
+import './rxjs-extensions';
+
+import { ChefPipe } from './courses/course/chef.pipe';
+import { DisableSelectedChefPipe } from './courses/course/disable-selected-chef.pipe';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
     HeaderNavbarComponent,
-    LoginComponent,
-    RoutingComponent,
-    UsersComponent
+    routableComponents,
+    ChefPipe,
+    DisableSelectedChefPipe
   ],
   imports: [
+    AlertModule,
+    DropdownModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    UsersModule,
+    CoursesModule,
+    RoutingModule,
   ],
-  providers: [],
+  providers: [
+    UsersService,
+    LoginService,
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/courses/course/course.module.ts
+++ src/app/courses/course/course.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseComponent } from './course.component';
 import { ChefPipe } from './chef.pipe';
+import { DisableSelectedChefPipe } from './disable-selected-chef.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ChefPipe]
+  declarations: [ChefPipe, DisableSelectedChefPipe]
 })
 export class CourseModule { }
