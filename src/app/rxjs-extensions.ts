b3fb08b6a947c83bc2b06e33ff214339d67a637a
Unknown <ruudn7@wp.pl>
2017-01-17 14:48:22 +0100

    rxjsCorrection

--- src/app/rxjs-extensions.ts
+++ src/app/rxjs-extensions.ts
@@ -2,5 +2,5 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/throw';
-import 'rxjs/add/operator/fromPromise';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/fromPromise';
